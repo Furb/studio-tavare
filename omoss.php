da5d0c8efeab4acace7c4c0a2cb7289b06638051
Furb <jthabo@gmail.com>
2023-02-17 17:25:28 +0100

    smaa fix på om oss

--- omoss.php
+++ omoss.php
@@ -18,7 +18,7 @@ Template Name: Om oss
    <img class="flex-1 lg:hidden" src="<?php the_field('portrett_bilde'); ?>" alt="">
    <?php endif; ?>
        
-   <p class="mb-8"><?php the_field('vi_er_hovedtekst'); ?></p>
+   <p class="mb-24"><?php the_field('vi_er_hovedtekst'); ?></p>
 
     </div>
 
@@ -42,8 +42,7 @@ Template Name: Om oss
 
       <img src="/wp-content/uploads/2023/02/kjersti.jpg" alt="Kjersti Bjerke">
       </div><!-- /.img-box -->
-    <p class="flex items-center">Kjersti Bjerke er utdannet fra Arkitektskolen Aarhus i 2011. Kjersti arbeider med et spekter av oppgaver innenfor landskapsarkitektur; fra konsept, stedsutvikling og regulering, til detaljprosjektering av byrom og spesielt kulturhistoriske anlegg. Hun var prosjektleder og fagansvarlig landskapsarkitekt i SLA, før etablering av Studio Tavare høsten 2022. Kjersti er en særlig sterk formgiver, som evner å se linjene fra den minste detaljen til den store by- og landskapsveven.
-</p>
+    <div class="flex items-center"><p><?php the_field('portrett_tekst_kjersti'); ?></p></div>
 
 <div class="max-w-2xl">
    <img src="/wp-content/uploads/2023/02/sunniva.jpg" alt="Sunniva Førland">
@@ -51,10 +50,8 @@ Template Name: Om oss
 </div><!-- /.img-box -->
 
 
+<div class="flex items-center"><p><?php the_field('portrett_tekst_sunniva'); ?></p></div><!-- /.flex items-center -->
 
-
-    <p class="flex items-center">Sunniva Førland er landskapsarkitekt mnla, utdannet fra Arkitekt- og Designhøyskolen i Oslo i 2012. Hun har omfattende erfaring med utforming av offentlige gater og byrom, og var ansvarlig for flere komplekse detaljprosjekt i SLA med mange involverte fagdisipliner. Sunniva har et skarpt blikk for det visuelle og estetiske, og evner å skape godt samarbeidsklima uansett prosjektfase og skala, fra byggeoppfølging til strategiske byplaner.  
-</p>
 
 
 
--- vivill.php
+++ vivill.php
@@ -8,17 +8,24 @@ Template Name: Vi vill
 <?php get_header(); ?>
 <section id="main" class="min-h-[50vh]">
    <div class="container mt-16 justify-between items-center mx-auto">
+      <div class="md:flex justify-between">
+      <div class="md:w-1/2">
+      
    
    <h2 class="mb-12"><?php the_field('underoverskrift'); ?></h2> 
 
-   <?php if( get_field('bilde_vi_vill') ): ?>
+   <p class="mb-8"><?php the_field('hovedtekst_vi_vill'); ?></p>
+
+   </div>
+   <div class="md:w-1/3 flex items-center">
+       <?php if( get_field('bilde_vi_vill') ): ?>
     
 
 
    <img class="mb-8" src="<?php the_field('bilde_vi_vill'); ?>" alt="">
    <?php endif; ?>
-
-   <p class="mb-8"><?php the_field('hovedtekst_vi_vill'); ?></p>
+   </div>
+   </div><!-- /.flex -->
 
 
     
