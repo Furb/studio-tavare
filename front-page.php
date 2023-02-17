5143fe6edc6cab181c190a5b5a822d57362744d8
Furb <jthabo@gmail.com>
2023-02-17 15:31:03 +0100

    front page responsive

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@ Template Name: Homepage
 <?php get_header(); ?>
 <section id="main" class="min-h-[50vh]">
    <div class="container mt-16 flex justify-between items-center mx-auto">
-    <div class="hidden lg:grid lg:grid-cols-4 lg:gap-8 xl:gap-12">
+    <div class="flex flex-col lg:grid lg:grid-cols-4 gap-8 xl:gap-12">
         <div class="col-span-2"><a href="/om-oss"><img src="/wp-content/uploads/2023/02/studio_tavare_om-oss.jpg" alt=""></a></div><!-- /.1 -->
          <div class="flex items-end"><img src="/wp-content/uploads/2023/02/logo_studio_tavare_moerk.png" alt=""></div><!-- /.2 -->
           <div class="3"><img src="/wp-content/uploads/2023/02/05_naernatur.jpg" alt=""><p class="label">Nærnatur</p><!-- /.label --></div><!-- /.3 -->
@@ -21,23 +21,7 @@ Template Name: Homepage
              <div class="col-span-2"><img src="/wp-content/uploads/2023/02/12_Sirkulaer_materialbruk.jpg" alt=""><p class="label">Sirkulær materialbruk</p><!-- /.label --></div><!-- /.10 -->
            
 </div><!-- /.flex flex-wrap md:flex-nowrap -->
-<div class="flex flex-col gap-8 lg:hidden">
-    <div><img src="/wp-content/uploads/2023/02/logo_studio_tavare_moerk.png" alt=""></div><!-- /.2 -->
-    <div class="flex items-center"><h3 class="pt-8">Studio Tavare arbeider i krysningspunktet mellom kunst, landskapsarkitektur og urbanisme.</h3></div><!-- /.4 -->
-        <div><img src="/wp-content/uploads/2023/02/studio_tavare_om-oss.jpg" alt=""></div><!-- /.1 -->
-         
-          <div><img src="/wp-content/uploads/2023/02/05_naernatur.jpg" alt=""></div><!-- /.3 -->
-            
-            <div><img src="/wp-content/uploads/2023/02/07_byrom_og_kulturarv.jpg" alt=""></div><!-- /.5 -->
-            <div><img src="/wp-content/uploads/2023/02/08_landskapsforstaaelse.jpg" alt=""></div><!-- /.6 -->
-            <div><img src="/wp-content/uploads/2023/02/09_prosess.jpg" alt=""></div><!-- /.7 -->
-            <div><img src="/wp-content/uploads/2023/02/10_vann.jpg" alt=""></div><!-- /.8 -->
-            <div><img src="/wp-content/uploads/2023/02/11_byliv.jpg" alt=""></div><!-- /.9 -->
-            <div><img src="/wp-content/uploads/2023/02/prosess_02.jpg" alt=""></div><!-- /.12 -->
-             <div><img src="/wp-content/uploads/2023/02/12_Sirkulaer_materialbruk.jpg" alt=""></div><!-- /.10 -->
-             
-           
-</div><!-- /.flex flex-wrap md:flex-nowrap -->
+
    </div><!-- /.container -->
 </section><!-- /#main.min-h- -->
 
