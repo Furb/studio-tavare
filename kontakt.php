88f3e777d484e6bf3c378b88e13551cabc85f8d7
Jesper <jthabo@gmail.com>
2025-09-30 22:05:46 +0200

    changed text on contact page

--- kontakt.php
+++ kontakt.php
@@ -8,28 +8,19 @@ Template Name: Kontakt
 <section id="main" class="min-h-[50vh]">
    <div class="container mt-16 justify-between items-center mx-auto">
     <div class="sm:grid grid-cols-2 xl:grid-cols-3 gap-4">
-          <h2 class="col-span-3">Studio Tavare er et lite, fleksibelt og uavhengig firma, med lang erfaring fra flere anerkjente landskapskontor i Norden. Kontakt oss, vi er alltid nysgjerrig og åpen for samarbeid.</h2>
+          <h2 class="col-span-3">Studio Tavare er et lite, fleksibelt og uavhengig firma, med lang erfaring fra flere anerkjente landskapskontor i Norden. Alltid nysgjerrig og åpen for samarbeid.</h2>
         <div class="card mb-12 md:mb-0">
             <img src="/wp-content/uploads/2023/02/kjersti.jpg" alt="Kjersti Bjerke">
             <ul class="info py-6 font-light">
                 <li>Kjersti Bjerke </li>
-                <li>Landskapsarkitekt mnla / partner</li>
+                <li>Landskapsarkitekt mnla</li>
                 <li>+ 47 452 64 248</li>
                 <li>kjersti@studiotavare.no</li>
             </ul><!-- /.info -->
             
         </div><!-- /.card -->
 
-        <div class="card">
-             <img src="/wp-content/uploads/2023/02/sunniva.jpg" alt="Sunniva Førland">
-            <ul class="info py-6 font-light">
-                <li>Sunniva Førland</li>
-                <li>Landskapsarkitekt mnla / partner/ daglig leder </li>
-                <li>+ 47 472 43 140</li>
-                <li>sunniva@studiotavare.no</li>
-            </ul><!-- /.info -->
-            
-        </div><!-- /.card -->
+      
 
       
 
